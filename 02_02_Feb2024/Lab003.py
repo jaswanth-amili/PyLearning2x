ccac849ebb68dbe51a2fbabce218b5c6edf29e6e
jaswanth-pc <jaswanth.amilineni@peppercloud.com>
2024-02-20 08:17:33 +0530

    Added 02Feb2024 file

---
+++ 02_02_Feb2024/Lab003.py
@@ -0,0 +1,14 @@
+# Keyword and Identifiers
+# Keyword - reserved words
+# Identifier - Variable
+
+age = 25
+print(age)
+
+name = "Jack"
+print(name)
+
+# Can't start with number
+
+_ = "Underscore"
+print(_)
---
+++ 02_02_Feb2024/Lab005.py
@@ -0,0 +1,6 @@
+print("Jack","and","Jill","Went","up","the","hill", sep=" ", end=" | ")
+print("Jack","and","Jill","Went","up","the","hill", sep=" ")
+
+# Built in functions
+Maximum = max(2,4,1,5,10,8,6)
+print(Maximum)
